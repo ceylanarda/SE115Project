d83bbd87af84af06e75fc302d367d8622e856991
ceylanarda <ceylan.arda2006@gmail.com>
2025-12-15 00:55:57 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,20 +9,82 @@ public class Main {
     static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
     static String[] months = {"January","February","March","April","May","June",
                               "July","August","September","October","November","December"};
-    
+
+    //data için arrayimiz.
+    public static int [] [] [] data=new int[MONTHS][DAYS][COMMS];
+
+
+    private static int getCommIndex(String commodity){
+        for(int i = 0;i<COMMS;i++){
+            if (commodities[i].equals(commodity)){//stringi inte eşitlemek için .equals.
+                return i;
+            }
+        }
+        return -1; //hata kodu
+    }
+
 
     // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
     public static void loadData() {
+        for(int m =0;m<MONTHS;m++){
+            String fileName= "Data_Files/" + months[m] + ".txt";
+            File file = new File(fileName);
+
+            try {
+                Scanner sc=new Scanner(System.in);
+                while (sc.hasNextLine()){
+                    String line = sc.nextLine();
+                    //20,Oil,3000 gibi olucak
+                    String[] parts = line.split(",");
+                    // Araya virgül koymak için.
+                    if(parts.length==3){
+                        int day = Integer.parseInt(parts[0].trim());//Boşlukları kaldırıcak.
+                        String namecomm = parts[1].trim();//Boşluk kaldırıcak isimdeki.
+                        int profit = Integer.parseInt(parts[2].trim());//Aynı şekilde
+
+                        int commindex = getCommIndex(namecomm);
+
+                        if(day>=1 && day < DAYS && commindex!=-1){
+                            data[m][day-1][commindex]=profit;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                // Exception olmaması ve göstermemesi için.
+            }
+        }
     }
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
     public static String mostProfitableCommodityInMonth(int month) {
-        return "DUMMY"; 
+        if(month<0 || month>=MONTHS){return "DUMMY";}
+
+        int maxProfit = Integer.MIN_VALUE; // En küçük sayı başlangıç say.
+        int bestCommindex = -1;
+
+        for(int c=0;c<COMMS;c++){ //Emtiaları gez
+            int currentCommtotal= 0;
+
+            for(int d =0;d<DAYS;d++){ //Her emtianın o ayki toplamı.
+                currentCommtotal+=data[month][d][c];
+            }
+            if(currentCommtotal>maxProfit){//Eğer maxtan fazlaysa yeni max o.
+                maxProfit=currentCommtotal;
+                bestCommindex=c;
+            }
+        }
+        return commodities[bestCommindex]+ " " + maxProfit;
     }
 
     public static int totalProfitOnDay(int month, int day) {
-        return 1234;
+        if(month<0 || month>=MONTHS || day<1 || day > DAYS){return 1234;}//geçersiz değer girmesi için.
+        int total = 0;
+
+        for(int c=0;c<COMMS;c++){//belli gün ve aydaki emtiaları toplar.
+            total += data[month][day-1][c];
+        }
+        return total;
     }
 
     public static int commodityProfitInRange(String commodity, int from, int to) {
